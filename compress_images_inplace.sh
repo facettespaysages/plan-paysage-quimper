437cc825833f46b2f00f4b047032823d2940e835
Thomas Pocreau <thomas.pocreau@blablacar.com>
2024-03-27 19:03:41 +0100

    Add script

--- compress_images_inplace.sh
+++ compress_images_inplace.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo 'Ce script optimise toutes les images de ce dossier (et les sous dossier) y compris les panoramas.'
+echo 'Toutes les photos de ce dossier seront écrasées.'
+read -p 'Voulez-vous continuer ? (Oui/non) ' yesno
+
+if ! [[ "$yesno" =~ ^Oui$ ]]
+then
+    echo "Abandon"
+    exit 0
+fi
 
 for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 150 *.HEIC); done
 for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 150 *.c); done
