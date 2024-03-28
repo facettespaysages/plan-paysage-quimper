7fcb9139d316e078fd33b991e994f7fd601e3d9b
Thomas Pocreau <thomas.pocreau@blablacar.com>
2024-03-28 09:11:04 +0100

    Add logs

--- compress_images_inplace.sh
+++ compress_images_inplace.sh
@@ -12,37 +12,37 @@ read -p 'Voulez-vous transformer tous les fichiers en HEIC pour un meilleur resu
 
 if ! [[ "$yesnoheic" =~ ^Oui$ ]]
 then
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.HEIC); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.c); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.JPEG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.jpeg); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.JPG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.jpg); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.PNG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 *.png); done
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.HEIC
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.heic
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.JPEG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.jpeg
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.JPG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.jpg
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.PNG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 *.png
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress HEIC in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.HEIC); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress heic in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.heic); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress JPEG in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.JPEG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress jpeg in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.jpeg); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress JPG in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.JPG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress jpg in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.jpg); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress PNG in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.PNG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress png in $d" && convert -resize 2560x2560 -quality 85 -density 72 *.png); done
+  convert -resize 2560x2560 -quality 85 -density 72 *.HEIC
+  convert -resize 2560x2560 -quality 85 -density 72 *.heic
+  convert -resize 2560x2560 -quality 85 -density 72 *.JPEG
+  convert -resize 2560x2560 -quality 85 -density 72 *.jpeg
+  convert -resize 2560x2560 -quality 85 -density 72 *.JPG
+  convert -resize 2560x2560 -quality 85 -density 72 *.jpg
+  convert -resize 2560x2560 -quality 85 -density 72 *.PNG
+  convert -resize 2560x2560 -quality 85 -density 72 *.png
 else
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.HEIC); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.c); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPEG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpeg); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpg); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.PNG); done
-  for d in ./*/ ; do (cd "$d" && magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.png); done
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.HEIC
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.heic
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPEG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpeg
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpg
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.PNG
-  magick mogrify -resize 2560x2560 -quality 85 -density 72 -format HEIC *.png
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress HEIC in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.HEIC); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress heic in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.heic); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress JPEG in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPEG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress jpeg in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpeg); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress JPG in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress jpg in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpg); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress PNG in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.PNG); done
+  for d in ./*/ ; do (cd "$d" && echo "Starting to compress png in $d" && convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.png); done
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.HEIC
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.heic
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPEG
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpeg
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.JPG
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.jpg
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.PNG
+  convert -resize 2560x2560 -quality 85 -density 72 -format HEIC *.png
 fi
